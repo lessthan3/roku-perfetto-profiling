1525e9b2464286bc93698c4c75591d445649fed0
Addison <addison@maestro.io>
2026-07-27 18:31:13 -0400

    update postinstall

--- bin/postinstall.sh
+++ bin/postinstall.sh
@@ -1,3 +1,9 @@
 #!/usr/bin/env bash
 
 [ -f .gitmodules ] && git submodule update --init
+
+# after install, perform setup steps
+cd maestro-crossplatform-testing || return 1
+
+yarn install
+yarn workspace @maestro-crossplatform/core build
